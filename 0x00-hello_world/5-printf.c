ac76c7fe90cc0bf17de7bb44167aae284108c9b8
olumide12-cell <olubimimichael01@gmail.com>
2022-09-08 10:19:59 -0700

    psh

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,9 +4,9 @@
  * Description: 'Output'
  *Return: Always 0 (success)
  */
-int main()
+int main(void)
 
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
- return (0);
+return (0);
 }
