ae422285b3a11370c996102ea229b1dee2638e69
olumide12-cell <olubimimichael01@gmail.com>
2022-09-09 06:03:58 -0700

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,8 @@
  * main - Entry point
  * Return: Always 0 (Success)
  */
-int main(void) //there are no argument taken by main
+int main(void)
+//there are no argument taken by main
 {
 int n;
 srand(time(0));
