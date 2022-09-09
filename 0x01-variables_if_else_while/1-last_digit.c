6842fa4623e8b82d585e9de1dab646b4aefcb296
olumide12-cell <olubimimichael01@gmail.com>
2022-09-09 08:52:50 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -27,7 +27,7 @@ else if (k == 0)
 }
  else
 {
-	printf("Last digit of %d is %d and less than 6 and not 0\n" n, k);
+printf("Last digit of %d is %d and less than 6 and not 0\n", n, k);
 }
  
 return (0);
