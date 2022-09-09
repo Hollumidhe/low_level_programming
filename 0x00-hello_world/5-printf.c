b21d9f337f07d4fe4c6cf6aa1b4e587e5e8610d7
olumide12-cell <olubimimichael01@gmail.com>
2022-09-08 20:17:58 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
  * this program prints "Programming is like a multillingual puzzle
  * Return: Always 0 (success)
  */
-int main(void)
+int main (void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
