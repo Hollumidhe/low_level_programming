f761e06b193893cd3c612cd4b03a05e45c0286da
olumide12-cell <olubimimichael01@gmail.com>
2022-09-09 12:44:50 -0700

    committ

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,17 +4,17 @@
 *
 * Return: Always 0 (Success)
 */
-int main(void)  
+int main(void)
 {
-char abc;  
-for (abc = 'a'; abc <= 'z'; abc++)    
-{  
+char abc;
+for (abc = 'a'; abc <= 'z'; abc++)   
+{
 if (abc == 'q' || abc == 'e')
-{	  
+{
 continue;
 }
-putchar(abc);      
-}  
-putchar('\n');  
+putchar(abc);
+}
+putchar('\n');
 return (0);
 }
