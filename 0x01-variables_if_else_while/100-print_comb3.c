5eb611a94d14568aebb9cdc94257edc5f3f5d503
olumide12-cell <olubimimichael01@gmail.com>
2022-09-10 10:50:53 -0700

    commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,22 +8,36 @@ int main(void)
 {
 int p;
 int k;
-for (p = 34;p<=46;p++)
+for (p = '0'; p < '9'; p++)
 {
-for (k = 35;k<=47;k++)
-{
-if (k > p)
-{
+for (k = '0'; <= '9'; k++)
+{    
+if (p == '0' && k <'1')
+continue
+if (p == '1' && k < '2')
+continue
+if (p == '2' && k < '3')
+continue
+if (p == '3' && k < '4')
+continue
+if (p == '4' && k < '5')
+continue
+if (p == '5' && k < '6')
+continue
+if (p == '6' && k < '7')
+continue
+if (p == '7' && k < '8')
+continue
+if (p == '8' && k < '9')
+continue
 putchar(p);
 putchar(k);
-if (p!=46 || k!=47)
-{
-putchar(',');
-putchar(',');
-}
-}
+if (p == '8' && k == '9')
+break;
+purchar(',');
+purchar(' ');
 }
 }
 putchar('\n');
 return (0);
-} 
+}
