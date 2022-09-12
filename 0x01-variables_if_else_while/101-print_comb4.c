fa65fd825e3ae876032021a1f89fa7ab15e72344
olumide12-cell <olubimimichael01@gmail.com>
2022-09-12 03:11:00 -0700

    commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,35 +1,30 @@
 #include <stdio.h>
 /**
- * main - program that prints al possible different combination of 2 digits
- * Return: 0
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	int c = 0;
-	int a;
-	int d;
-	
-	while (c <= 99)
-	{
-		a = (c / 10 + '0');
-		d = (c % 10 + '0');
-
-		if (a < d)
-		{
-			putchar(a);
-			putchar(d);
-
-			if (c != 89)
-			{
-				putchar(',');
-				putchar(' ');
-
-
-			}
-
-		}
-		c++;
-	}
+int nums;
+int nums1;
+int nums2;
+for (nums = '0'; nums <= '7'; nums++)
+{
+for (nums1 = nums + 1; nums1 <= '9'; nums1++)
+{
+for (nums2 = nums1 + 1; nums2 <= '9'; nums2++)
+{
+putchar(nums);
+putchar(nums1);
+putchar(nums2);
+if (nums == '7' && nums1 == '8' && nums2 == '9')
+break;
+putchar(',');
+putchar(' ');	      
+}
+}
+}
 putchar('\n');
-return (0);
+return (0); 
 }
