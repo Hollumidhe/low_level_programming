3032a10fa9b4c60715961228659c168f435f090d
olumide12-cell <olubimimichael01@gmail.com>
2022-09-13 09:12:39 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,9 +2,10 @@
 /**
  * print_alphabet_10x - function to print abc 10 times
  *
- * Return: 0
+ * Return:Always 0 (success)
  */
-void print_alphabet_x10(void)
+
+void print_alphabet_x10()
 {
 char d, f;
 for (f = 0; f <= 9; f++)
