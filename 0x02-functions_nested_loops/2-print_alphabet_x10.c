d93e9ea42d74924a86f41609063ac619d4ea0f61
olumide12-cell <olubimimichael01@gmail.com>
2022-09-13 09:08:09 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_alphabet_10x - function to print abc 10 times
  *
@@ -7,24 +6,13 @@
  */
 void print_alphabet_x10(void)
 {
-	char d, f;
-
-
-
-	for (f = 0; f <= 9; f++)
-
-	{
-
-		for (d = 'a'; d <= 'z'; d++)
-
-		{
-
-			_putchar(d);
-
-		}
-
-		_putchar('\n');
-
-	}
-
+char d, f;
+for (f = 0; f <= 9; f++)
+{
+for (d = 'a'; d <= 'z'; d++)
+{
+_putchar(d);
+}
+_putchar('\n');
+}
 }
