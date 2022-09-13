ada8b214b34095e8c8c80d9e26797ddd47d6583e
olumide12-cell <olubimimichael01@gmail.com>
2022-09-13 08:45:12 -0700

    #include
    /**
    
     *print_alphabet_10x - function to print abc 10 times
    
     *
    
     * Return: 0
    
     */
    
    void print_alphabet_x10(void)
    
    {
    
    char d, f;
    
    for (f = 0; f <= 9; f++)
    
    {
    
    for (d = a; d <= z; d++)
    
    {
    
    _putchar(d);
    
    }
    
    _putchar(n);
    
    }
    
    }

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,37 @@
+#include
+
+/**
+
+ *print_alphabet_10x - function to print abc 10 times
+
+ *
+
+ * Return: 0 
+
+ */
+
+void print_alphabet_x10(void)
+
+{
+
+	char d, f;
+
+
+
+	for (f = 0; f <= 9; f++)
+
+	{
+
+		for (d = 'a'; d <= 'z'; d++)
+
+		{
+
+			_putchar(d);
+
+		}
+
+		_putchar('\n');
+
+	}
+
+}
