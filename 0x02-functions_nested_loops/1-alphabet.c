be1926f5a75f7489c2786dd8f2050c3b68919d72
olumide12-cell <olubimimichael01@gmail.com>
2022-09-13 06:50:29 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - funtion that print alphabeth in lower case
+ * 
+ * Return: Always 0
+
+ */
+void print_alphabet(void)
+}
+	char e;
+
+	for (e = 'a'; e <= '2'; e++)
+	{
+		putchar (e);
+	}
+
+	_putchar('\n');
+}
