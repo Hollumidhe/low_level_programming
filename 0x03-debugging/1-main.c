1bef2d6694f1ce7f302d5711b4091bf6b4fb4912
olumide12-cell <olubimimichael01@gmail.com>
2022-09-15 05:18:07 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -3,13 +3,10 @@
 * main - causes an infinite loop
 * Return: 0
 */
-
 int main(void)
 {
 int i;
-
 printf("Infinite loop incoming :(\n");
-
 i = 0;
 /*
 * while (i < 10)
@@ -18,6 +15,5 @@ i = 0;
 * }
 */
 printf("Infinite loop avoid! \\o/\n");
-
 return (0);
 }
