47ded021e90f7ab5dc06d06c9075f2ee570df0de
olumide12-cell <olubimimichael01@gmail.com>
2022-10-13 04:55:04 -0700

    function pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,9 +5,9 @@
 * @size: size of array
 * @action: function pointer
 */
-void array_iterator(int *array, size_t size, void (*action)(int));
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
 size_t i;
-for (i = 0; i < size; i++);
+for (i = 0; i < size; i++)
 action(array[i]);
 }
