556a31726a63ee8502489fd5d8afdfa527403348
olumide12-cell <olubimimichael01@gmail.com>
2022-10-13 04:22:58 -0700

    Function that prints a name.

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,7 +4,7 @@
 * @name: char string
 * @f: function pointer that takes a string argument
 */
-void print_name (char *name, (*f)(char *));
+void print_name (char *name, void (*f)(char *));
 {
 f(name);
 }
