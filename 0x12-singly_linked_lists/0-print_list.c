fe811a07278d42911740292b65f59c17665aa0be
olumide12-cell <olubimimichael01@gmail.com>
2022-10-20 18:44:59 -0700

    task added

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+* print_list - will print all elements at list_t
+* @h: singly linked list
+* Return: number of elements in the list.
+*/
+size_t print_list(const list_t *h)  
+{  
+size_t dk;  
+dk = 0;  
+while (h != NULL)    
+{      
+if (h->str == NULL)	
+{	  
+printf("[0] (nil)\n");	  
+h = h->next;	  
+dk++;	  
+}      
+printf("[%d] %s\n", h->len, h->str);      
+h = h->next;
+dk++;
+}
+return (dk);
+}
