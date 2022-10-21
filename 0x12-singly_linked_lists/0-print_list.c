ba26b7c925d595b35db8352f88cbca43661dc64f
olumide12-cell <olubimimichael01@gmail.com>
2022-10-20 19:35:37 -0700

    0 task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,18 +4,18 @@
 * @h: pointer to the list address
 * Return: number of nodes
 */
-size_t print_list(const list_t *h)  
-{  
-unsigned int i;  
-if (h == NULL)    
+size_t print_list(const list_t *h)
+{
+unsigned int i;
+if (h == NULL)
 return (0);
-for (i = 0; h != NULL; i++)    
-{      
-if (h->str == NULL)	
-printf("[0] (nil)\n");      
-else	
-printf("[%d] %s\n", h->len, h->str);      
-h = h->next;      
-}  
+for (i = 0; h != NULL; i++)
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%d] %s\n", h->len, h->str);
+h = h->next;
+}
 return (i);
 }
